dnlopez/PyGamebase

commit b601838985797f1cdba6a0a4870b58b2b416ca70

Work with screenshots and images in zip files

frontend/detail_pane.py
@@ -3,6 +3,17 @@
 import os.path
 import urllib.parse
 import math
+import base64
+import io
+import tempfile
+import zipfile
+
+# Pillow
+try:
+    import PIL.Image
+    g_havePil = True
+except ImportError:
+    g_havePil = False
 
 # Qt
 from PySide2.QtCore import *
@@ -219,6 +230,32 @@ class DetailPane(QWidget):
         if "detailPaneStylesheet" in settings.preferences and settings.preferences["detailPaneStylesheet"] != "":
             html += '  <link rel="stylesheet" type="text/css" href="file://' + (settings.preferences["detailPaneStylesheet"]).replace("\\", "/") + '">\n'
 
+        def extraIsAnImage(i_extrasRow):
+            """
+            Params:
+             i_extrasRow:
+              (dict)
+
+            Returns:
+             (bool)
+            """
+            # If "Path" is an image,
+            # return True
+            path = i_extrasRow["Path"]
+            if path != None:
+                if path.lower().endswith(".jpg") or path.lower().endswith(".jpeg") or path.lower().endswith(".gif") or path.lower().endswith(".png"):
+                    return True
+                # Else if "Path" is a zip, we can support images inside it
+                elif path.lower().endswith(".zip"):
+                    # If "FileToRun" is an image,
+                    # return True
+                    fileToRun = i_extrasRow["FileToRun"]
+                    if fileToRun != None:
+                        if fileToRun.lower().endswith(".jpg") or fileToRun.lower().endswith(".jpeg") or fileToRun.lower().endswith(".gif") or fileToRun.lower().endswith(".png"):
+                            return True
+            #
+            return False
+
         for item in g_detailPaneItems:
             if item == "Screenshots (all)":
                 html += '  <div id="screenshots">\n'
@@ -381,13 +418,7 @@ class DetailPane(QWidget):
 
             elif item == "Non-image extras":
                 # Get only the extras which aren't images
-                nonImageRows = []
-                for extrasRow in extrasRows:
-                    path = extrasRow["Path"]
-                    if path != None and (path.lower().endswith(".jpg") or path.lower().endswith(".jpeg") or path.lower().endswith(".gif") or path.lower().endswith(".png")):
-                        pass
-                    else:
-                        nonImageRows.append(extrasRow)
+                nonImageRows = [extrasRow  for extrasRow in extrasRows  if not extraIsAnImage(extrasRow)]
 
                 # For each non-image, insert a link
                 if len(nonImageRows) > 0:
@@ -415,11 +446,7 @@ class DetailPane(QWidget):
 
             elif item == "Image extras":
                 # Get only the extras which are images
-                imageRows = []
-                for extrasRow in extrasRows:
-                    path = extrasRow["Path"]
-                    if path != None and (path.lower().endswith(".jpg") or path.lower().endswith(".jpeg") or path.lower().endswith(".gif") or path.lower().endswith(".png")):
-                        imageRows.append(extrasRow)
+                imageRows = [extrasRow  for extrasRow in extrasRows  if extraIsAnImage(extrasRow)]
 
                 # For each image extra, insert an image
                 if len(imageRows) > 0:
@@ -436,7 +463,41 @@ class DetailPane(QWidget):
                         html += '<a href="' + url + '" style="display: inline-block; text-align: center;">'
 
                         if hasattr(gamebase.adapters[i_adapterId]["module"], "config_extrasBaseDirPath"):
-                            html += '<img src="file://' + gamebase.normalizeDirPathFromAdapter(gamebase.adapters[i_adapterId]["module"].config_extrasBaseDirPath) + "/" + imageRow["Path"] + '" style="height: 300px;">'
+                            if imageRow["Path"].lower().endswith(".zip"):
+                                zipFilePath = gamebase.normalizeDirPathFromAdapter(gamebase.adapters[i_adapterId]["module"].config_extrasBaseDirPath) + os.sep + imageRow["Path"]
+                                memberPath = imageRow["FileToRun"]
+                                if g_havePil:
+                                    # Load the file from the zip into memory
+                                    imageBytes = gamebase.getZipMemberBytes(zipFilePath, memberPath)
+
+                                    # Parse image,
+                                    # convert colours to RGB
+                                    # and save as JPEG, to make it smaller and mostly come within QtWebEngine/Chromium data URI size limits
+                                    image = PIL.Image.open(io.BytesIO(imageBytes))
+                                    #image = image.resize((image.width // 8, image.height // 8))
+                                    if image.mode != "RGB":
+                                        image = image.convert("RGB")
+                                    imageBytesIo = io.BytesIO()
+                                    image.save(imageBytesIo, format="JPEG")
+                                    imageBytes = imageBytesIo.getvalue()
+
+                                    # Convert to data URI
+                                    encoded = base64.b64encode(imageBytes)
+                                    imgSrc = "data:image/jpeg;base64," + str(encoded.decode())
+                                else:
+                                    # (Re-)create temporary directory
+                                    # and extract image file to it
+                                    tempDirPath = tempfile.gettempdir() + "/gamebase/images/" + os.path.basename(zipFilePath) + "/"
+                                    frontend_utils.createTree(tempDirPath)
+                                    zipFile = zipfile.ZipFile(zipFilePath)
+                                    zipFile.extract(memberPath, tempDirPath)
+
+                                    #
+                                    imgSrc = tempDirPath + os.sep + memberPath
+                            else:
+                                imgSrc = 'file://' + gamebase.normalizeDirPathFromAdapter(gamebase.adapters[i_adapterId]["module"].config_extrasBaseDirPath) + "/" + imageRow["Path"]
+
+                            html += '<img src="' + imgSrc + '" style="height: 300px;">'
                         #html += '<img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png" style="height: 300px;">'
                         html += '<div>' + imageRow["Name"] + '</div>'
 
@@ -449,6 +510,8 @@ class DetailPane(QWidget):
 
 
         #print(html)
+        #with open("/tmp/detail_pane.html", "w") as f:
+        #    f.write(html)
 
         #self.webEngineView.setHtml(html, QUrl("file:///"))
         # Load HTML into a QWebEnginePage with a handler for link clicks

frontend/frontend_utils.py
@@ -1,6 +1,7 @@
 
 # Python std
 import sys
+import os
 
 
 def openInDefaultApplication(i_filePaths):
@@ -43,3 +44,18 @@ def openInDefaultApplication(i_filePaths):
     popen = subprocess.Popen(executableAndArgs,
                              shell=True,
                              stdout=sys.stdout.fileno(), stderr=sys.stderr.fileno())
+
+def createTree(i_pathOnDiskOfLeafDir):
+    """
+    Params:
+     i_pathOnDiskOfLeafDir:
+      (str)
+    """
+    # Allow either forward or back-slashes
+    i_pathOnDiskOfLeafDir = i_pathOnDiskOfLeafDir.replace("/", os.sep)
+
+    partialPath = ""
+    for part in i_pathOnDiskOfLeafDir.split(os.sep):
+        partialPath += part + os.sep
+        if not os.path.isdir(partialPath):
+            os.mkdir(partialPath)

frontend/game_table_view.py
@@ -3,6 +3,9 @@ import sqlite3
 import functools
 import copy
 import random
+import os
+import zipfile
+import tempfile
 
 # Qt
 from PySide2.QtCore import *
@@ -15,7 +18,7 @@ import qt_extras
 import columns
 import db
 import frontend_utils
-import utils
+#import utils
 import sql
 import gamebase
 import settings
@@ -185,7 +188,15 @@ class MyStyledItemDelegate(QStyledItemDelegate):
 
             screenshotFullPath = gamebase.dbRow_nthScreenshotFullPath(self.parent().dbRows[i_index.row()], picNo)
             if screenshotFullPath != None:
-                pixmap = QPixmap(screenshotFullPath)
+                zipExtensionPos = screenshotFullPath.lower().find(".zip/")
+                if zipExtensionPos == -1:
+                    pixmap = QPixmap(screenshotFullPath)
+                else:
+                    zipFilePath = screenshotFullPath[:zipExtensionPos + 4]
+                    memberPath = screenshotFullPath[zipExtensionPos + 5:]
+
+                    pixmap = QPixmap()
+                    pixmap.loadFromData(gamebase.getZipMemberBytes(zipFilePath, memberPath))
                 destRect = danrectToQrect(fitLetterboxed(qrectToDanrect(pixmap.rect()), qrectToDanrect(i_option.rect)))
                 i_painter.drawPixmap(destRect, pixmap)
 
@@ -690,7 +701,22 @@ class GameTableView(QTableView):
             rowNo = i_modelIndex.row()
             screenshotFullPath = gamebase.dbRow_nthScreenshotFullPath(self.dbRows[rowNo], picNo)
             if screenshotFullPath != None:
-                frontend_utils.openInDefaultApplication(screenshotFullPath)
+                zipExtensionPos = screenshotFullPath.lower().find(".zip/")
+                if zipExtensionPos == -1:
+                    frontend_utils.openInDefaultApplication(screenshotFullPath)
+                else:
+                    zipFilePath = screenshotFullPath[:zipExtensionPos + 4]
+                    memberPath = screenshotFullPath[zipExtensionPos + 5:]
+
+                    # (Re-)create temporary directory
+                    # and extract image file to it
+                    tempDirPath = tempfile.gettempdir() + "/gamebase/images"
+                    frontend_utils.createTree(tempDirPath)
+                    zipFile = zipfile.ZipFile(zipFilePath)
+                    zipFile.extract(memberPath, tempDirPath)
+
+                    #
+                    frontend_utils.openInDefaultApplication(tempDirPath + os.sep + memberPath)
 
         # Random screenshot
         elif columnId.startswith("random_pic[") and columnId.endswith("]"):

frontend/gamebase.py
@@ -487,3 +487,32 @@ def dbRow_photoFullPath(i_row):
 # + + }}}
 
 # + }}}
+
+# + Zip file cache {{{
+
+import zipfile
+g_zipFiles = {}
+
+def getZipMemberBytes(i_zipFilePath, i_memberPath):
+    """
+    Params:
+     i_zipFilePath:
+      (str)
+     i_memberPath:
+      (str)
+
+    Returns:
+     (bytes)
+    """
+    # If zip file isn't already open,
+    # open it
+    global g_zipFiles
+    if i_zipFilePath not in g_zipFiles:
+        g_zipFiles[i_zipFilePath] = zipfile.ZipFile(i_zipFilePath, "r")
+
+    #
+    zipFile = g_zipFiles[i_zipFilePath]
+
+    return zipFile.read(i_memberPath)
+
+# + }}}

commit fc42bd06272bf1300641979cdc10236ab4fe39f8

Fix bug in detail pane when click on extra without a FileToRun
Probably introduced with commit b601838, "Work with screenshots and
images in zip files".

frontend/detail_pane.py
@@ -530,17 +530,21 @@ class DetailPane(QWidget):
                     # pass it to the adapter file's runExtra()
                     url = i_qUrl.toString()
                     if url.startswith("extra:///"):
+                        # Strip protocol scheme
                         path = url[9:]
+                        # Get and unquote schema name, and rest expected to be extra paths
                         schemaName, extraPaths = path.split(":", 1)
                         schemaName = urllib.parse.unquote(schemaName)
+                        # Get and unquote single extra path, or file to run also
                         if extraPaths.find(":") == -1:
                             extraPath = extraPaths
+                            extraPath = urllib.parse.unquote(extraPath)
                             fileToRun = None
                         else:
                             extraPath, fileToRun = extraPaths.split(":", 1)
-                        extraPath = urllib.parse.unquote(extraPath)
-                        fileToRun = urllib.parse.unquote(fileToRun)
-
+                            extraPath = urllib.parse.unquote(extraPath)
+                            fileToRun = urllib.parse.unquote(fileToRun)
+                        #
                         extraInfo = [row  for row in self.extrasRows  if row["Path"] == extraPath][0]
                         gameInfo = self.gameRow
                         gameInfo = db.DbRecordDict(gameInfo)
@@ -558,18 +562,24 @@ class DetailPane(QWidget):
                     # Else if it's a link to a screenshot,
                     # open it with the default application
                     elif url.startswith("screenshot:///"):
+                        # Strip protocol scheme
                         path = url[14:]
+                        # Get and unquote schema name, and rest expected to be screenshot path
                         schemaName, screenshotPath = path.split("/", 1)
                         schemaName = urllib.parse.unquote(schemaName)
+                        #
                         screenshotPath = urllib.parse.unquote(screenshotPath)
                         frontend_utils.openInDefaultApplication(gamebase.screenshotPath_relativeToAbsolute(i_adapterId, screenshotPath))
 
                     # If it's a link to a game,
                     # select it in the table view
                     elif url.startswith("game:///"):
+                        # Strip protocol scheme
                         path = url[8:]
+                        # Get and unquote schema name, and rest expected to be game ID
                         schemaName, gameId = path.split("/", 1)
                         schemaName = urllib.parse.unquote(schemaName)
+                        #
                         gameId = urllib.parse.unquote(gameId)
                         gameId = int(gameId)
                         self.parent().parent().requestGameNavigation.emit(schemaName, gameId)